47f3336b9ee70edade103a3f4febfe2063cd02cc
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2020-02-06 11:14:08 -0800

    Events are broken. Need to see how they are working on netlify.

--- src/cms/preview-templates/EventPostPreview.js
+++ src/cms/preview-templates/EventPostPreview.js
@@ -3,12 +3,11 @@ import PropTypes from "prop-types";
 import { EventPostTemplate } from "../../templates/event-post";
 
 const EventPostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(["data", "tags"]);
+  console.log(entry);
   return (
     <EventPostTemplate
       content={widgetFor("body")}
       description={entry.getIn(["data", "description"])}
-      tags={tags && tags.toJS()}
       title={entry.getIn(["data", "title"])}
     />
   );
