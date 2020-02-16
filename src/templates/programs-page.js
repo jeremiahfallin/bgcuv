58f01bd3ee23a2758d1c3adc62b81bbcd136615b
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2020-02-16 12:25:33 -0800

    Added dono button to more heroes.

--- src/templates/programs-page.js
+++ src/templates/programs-page.js
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
+
 import Layout from "../components/Layout";
+import Hero from "../components/Hero";
 import Content, { HTMLContent } from "../components/Content";
 
 export const ProgramsPageTemplate = ({
@@ -17,40 +19,7 @@ export const ProgramsPageTemplate = ({
 
   return (
     <div>
-      <div
-        className="full-width-image margin-top-0"
-        style={{
-          backgroundImage: `url(${
-            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-          })`,
-          backgroundPosition: `top left`,
-          backgroundAttachment: `fixed`
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            height: "150px",
-            lineHeight: "1",
-            justifyContent: "space-around",
-            alignItems: "left",
-            flexDirection: "column"
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-            style={{
-              boxShadow: "#0081c6 0.5rem 0px 0px, #0081c6 -0.5rem 0px 0px",
-              backgroundColor: "#0081c6",
-              color: "white",
-              lineHeight: "1",
-              padding: "0.25em"
-            }}
-          >
-            {heading}
-          </h1>
-        </div>
-      </div>
+      <Hero {...{ image, title: heading }} />
       <section className="section section--gradient">
         <div className="container">
           <div className="columns">
